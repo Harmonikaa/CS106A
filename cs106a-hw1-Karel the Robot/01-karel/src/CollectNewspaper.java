d9121107f1e57357b6b4b47280f9c6566ce1480d
Harmonika <zyf3311@foxmail.com>
2016-08-12 16:28:44 +0800

    HW1 Problem1 Solved

--- cs106a-hw1-Karel the Robot/01-karel/src/CollectNewspaper.java
+++ cs106a-hw1-Karel the Robot/01-karel/src/CollectNewspaper.java
@@ -1,5 +1,6 @@
 /*
- * TODO: comment this program
+ * My code for Problem 1: CollectNewspaper
+ * Karel go out of room, pick up newspaper and then return to the starting point
  */
 
 import stanford.karel.*;
@@ -7,6 +8,29 @@ import stanford.karel.*;
 public class CollectNewspaper extends SuperKarel {
 	
 	public void run() {
-		// TODO: write the code to implement this program
+		MoveToTheNewspaper();
+		pickBeeper();
+		ReturnToStartingPoint();
+	}
+	
+	//Go from starting point to where the newspaper is placed
+	private void MoveToTheNewspaper() {
+		move();
+		move();
+		turnRight();
+		move();
+		turnLeft();
+		move();
+	}
+	
+	//Move back to starting point, facing east
+	private void ReturnToStartingPoint() {
+		turnAround();
+		move();
+		move();
+		move();
+		turnRight();
+		move();
+		turnRight();
 	}
 }
