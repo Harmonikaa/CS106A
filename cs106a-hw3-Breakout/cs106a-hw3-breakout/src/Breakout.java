923ed9400e9cc778e51241d05188c960b9be922a
Harmonika <zyf3311@foxmail.com>
2016-08-13 19:24:58 +0800

    HW3 starter file

---
+++ cs106a-hw3-Breakout/cs106a-hw3-breakout/src/Breakout.java
@@ -0,0 +1,19 @@
+// TODO: comment this program
+
+import acm.graphics.*;     // GOval, GRect, etc.
+import acm.program.*;      // GraphicsProgram
+import acm.util.*;         // RandomGenerator
+import java.applet.*;      // AudioClip
+import java.awt.*;         // Color
+import java.awt.event.*;   // MouseEvent
+
+public class Breakout extends BreakoutProgram {
+	
+	public void run() {
+		// Set the window to a nice size for our game
+		setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
+		
+		// TODO: finish this program
+	}
+	
+}
