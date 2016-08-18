a92cae0ff69425f569c583810163cf5f359da202
Harmonika <zyf3311@foxmail.com>
2016-08-18 10:40:21 +0800

    HW5 Completed

--- cs106a-hw5-MelodyAndImageAlgorithms/cs106a-hw5-melody-images/src/ImageAlgorithms.java
+++ cs106a-hw5-MelodyAndImageAlgorithms/cs106a-hw5-melody-images/src/ImageAlgorithms.java
@@ -6,34 +6,131 @@ import acm.graphics.*;
 public class ImageAlgorithms implements ImageAlgorithmsInterface {
 	public void grayscale(GImage source) {
 		// TODO: implement this method
+		int [][] pixels = source.getPixelArray();
+		for(int i=0;i<pixels.length;++i){
+			for(int j=0;j<pixels[0].length;++j){
+				int r = GImage.getRed(pixels[i][j]);
+				int g = GImage.getGreen(pixels[i][j]);
+				int b = GImage.getBlue(pixels[i][j]);
+				
+				int gray = (r+g+b)/3;
+				int pixel = GImage.createRGBPixel(gray, gray, gray);
+				pixels[i][j] = pixel;
+			}
+		}
+		source.setPixelArray(pixels);
 		
 	}
 
 	public void negative(GImage source) {
 		// TODO: implement this method
-		
+		int [][] pixels = source.getPixelArray();
+		for(int i=0;i<pixels.length;++i){
+			for(int j=0;j<pixels[0].length;++j){
+				int r = GImage.getRed(pixels[i][j]);
+				int g = GImage.getGreen(pixels[i][j]);
+				int b = GImage.getBlue(pixels[i][j]);
+				
+				int pixel = GImage.createRGBPixel(255-r, 255-g, 255-b);
+				pixels[i][j] = pixel;
+			}
+		}
+		source.setPixelArray(pixels);
 	}
 
 	public void rotateLeft(GImage source) {
 		// TODO: implement this method
+		int [][] pixels_origin = source.getPixelArray();
+		int height_origin = pixels_origin.length;
+		int width_origin = pixels_origin[0].length;
+		int [][] pixels_new = new int [width_origin][height_origin];
 		
+		for(int i=0; i<width_origin; ++i){
+			for(int j=0; j<height_origin; ++j){
+				pixels_new[i][j] = pixels_origin[j][width_origin-i-1];
+			}
+		}
+		source.setPixelArray(pixels_new);
 	}
 
 	public void rotateRight(GImage source) {
 		// TODO: implement this method
+		int [][] pixels_origin = source.getPixelArray();
+		int height_origin = pixels_origin.length;
+		int width_origin = pixels_origin[0].length;
+		int [][] pixels_new = new int [width_origin][height_origin];
 		
+		for(int i=0; i<width_origin; ++i){
+			for(int j=0; j<height_origin; ++j){
+				pixels_new[i][j] = pixels_origin[height_origin-j-1][i];
+			}
+		}
+		source.setPixelArray(pixels_new);
 	}
 	
 	public void translate(GImage source, int dx, int dy) {
 		// TODO: implement this method
+		int [][] pixels_origin = source.getPixelArray();
+		int height_origin = pixels_origin.length;
+		int width_origin = pixels_origin[0].length;
+		int [][] pixels_new = new int [height_origin][width_origin];
 		
+		for(int i=0;i<height_origin;++i){
+			for(int j=0;j<width_origin;++j){
+				int i_new = (i + dy)%height_origin;
+				while(i_new<0)
+					i_new += height_origin;
+				int j_new = (j + dx)%width_origin;
+				while(j_new<0)
+					j_new += width_origin;
+				pixels_new[i_new][j_new] = pixels_origin[i][j];
+			}
+		}
+		
+		source.setPixelArray(pixels_new);
 	}
 
 	public void blur(GImage source) {
 		// TODO: implement this method
+		int [][] pixels_origin = source.getPixelArray();
+		int height_origin = pixels_origin.length;
+		int width_origin = pixels_origin[0].length;
+		int [][] pixels_new = new int [height_origin][width_origin];
 		
+		for(int i=0;i<height_origin;++i){
+			for(int j=0;j<width_origin;++j){
+				blurPixel(i,j,pixels_origin,pixels_new);
+			}
+		}
+		
+		source.setPixelArray(pixels_new);
 	}
 
+	private void blurPixel(int i, int j, int [][]pixels_origin, int[][]pixels_new){
+		int height = pixels_origin.length;
+		int width = pixels_origin[0].length;
+		int r=0, g=0, b=0;
+		int pixel_cnt = 0;
+		
+		for(int row=i-1;row<=i+1;++row){			
+			if(row < 0 || row >=height)
+				continue;
+			for(int col=j-1;col<=j+1;++col){
+				if(col < 0 || col >= width)
+					continue;
+				++pixel_cnt;
+				r += GImage.getRed(pixels_origin[row][col]);
+				g += GImage.getGreen(pixels_origin[row][col]);
+				b += GImage.getBlue(pixels_origin[row][col]);				
+			}			
+			
+		}
+		r /= pixel_cnt;
+		g /= pixel_cnt;
+		b /= pixel_cnt;
+		int pixel = GImage.createRGBPixel(r, g, b);
+		pixels_new[i][j] = pixel;
+	}
     public void mystery(GImage source) {
         // TODO: (optional) have some fun!
     }
--- cs106a-hw5-MelodyAndImageAlgorithms/cs106a-hw5-melody-images/src/Melody.java
+++ cs106a-hw5-MelodyAndImageAlgorithms/cs106a-hw5-melody-images/src/Melody.java
@@ -6,53 +6,149 @@ import stanford.cs106.audio.*;
 
 public class Melody implements MelodyInterface {
 
+	private String str_Title;
+	private String str_Artist;
+	private int notes_cnt;
+	private Note [] notes;
+	private double totalDuration = 0;
+	
 	public Melody(Scanner input) {
 		// TODO: implement this constructor
+		str_Title = input.nextLine();
+		str_Artist = input.nextLine();
+		notes_cnt = Integer.parseInt(input.nextLine());
+		notes = new Note[notes_cnt];
+		
+		int cnt = 0;
+		while(input.hasNextLine()){
+			notes[cnt] = new Note(input.nextLine());
+			++cnt;
+		}
 		
 	}
 	
 	public void changeDuration(double ratio) {
 		// TODO: implement this method
+		for(int i=0;i<notes_cnt;++i){
+			double duration = notes[i].getDuration();
+			duration = duration * ratio;
+			notes[i].setDuration(duration);
+		}
+		totalDuration = totalDuration * ratio;
 
 	}
 	
 	public String getArtist() {
 		// TODO: implement this method
-		return "";
+		return str_Artist;
 	}
 
 	public String getTitle() {
 		// TODO: implement this method
-		return "";
+		return str_Title;
 	}
 	
 	public double getTotalDuration() {
 		// TODO: implement this method
-		return 0.0;
+		if(totalDuration == 0) {
+			int repeat_start = 0, repeat_end = 0;
+			boolean isRepeated = false;
+			for(int i=0; i<notes_cnt; ++i){
+				totalDuration += notes[i].getDuration();
+				if(notes[i].isRepeat() && !isRepeated){
+					isRepeated = true;
+					repeat_start = i;
+					continue;
+				}
+				if(notes[i].isRepeat() && isRepeated){
+					isRepeated = false;
+					repeat_end = i;
+					for(int j=repeat_start; j<=repeat_end;++j){
+						totalDuration += notes[j].getDuration();
+					}
+				}
+			}
+		}
+		return totalDuration;
 	}
 	
 	public boolean octaveDown() {
 		// TODO: implement this method
-		return false;
+		boolean isOctDownAvailable = true;
+		for(int i=0;i<notes_cnt;++i){
+			if(notes[i].getOctave() == 1){
+				isOctDownAvailable = false;
+				break;
+			}			
+		}
+		if(isOctDownAvailable){
+			for(int i=0;i<notes_cnt;++i){
+				int octave = notes[i].getOctave();
+				notes[i].setOctave(octave-1);
+			}
+			return true;
+		} else
+			return false;
 	}
 	
 	public boolean octaveUp() {
 		// TODO: implement this method
-		return false;
+		boolean isOctUpAvailable = true;
+		for(int i=0;i<notes_cnt;++i){
+			if(notes[i].getOctave() == 10){
+				isOctUpAvailable = false;
+				break;
+			}			
+		}
+		if(isOctUpAvailable){
+			for(int i=0;i<notes_cnt;++i){
+				int octave = notes[i].getOctave();
+				notes[i].setOctave(octave+1);
+			}
+			return true;
+		} else
+			return false;
 	}
 	
 	public void play() {
 		// TODO: implement this method
+		int repeat_start = 0, repeat_end = 0;
+		boolean isInRepeat = false;
+		
+		for(int i=0; i<notes_cnt; ++i){
+			notes[i].play();
+			if(notes[i].isRepeat() && !isInRepeat){
+				isInRepeat = true;
+				repeat_start = i;
+				continue;
+			}
+			
+			if(notes[i].isRepeat() && isInRepeat){
+				isInRepeat = false;
+				repeat_end = i;
+				for(int j=repeat_start; j<=repeat_end; ++j){
+					notes[j].play();
+				}
+			}
+			
+		}
 		
 	}
 	
 	public void reverse() {
 		// TODO: implement this method
+		Note buffer;
+		for(int i=0; i<notes_cnt/2;++i){
+			int j = notes_cnt-1-i;
+			buffer = notes[i];
+			notes[i] = notes[j];
+			notes[j] = buffer;			
+		}
 		
 	}
 	
 	public String toString() {
 		// TODO: implement this method (optional)
-		return "";
+		return Arrays.toString(notes);
 	}
 }
